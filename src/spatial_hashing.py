da0e02402bacd35af49ec0884129676ba8361f15
Thomas Theisen <32493141+TTheisen@users.noreply.github.com>
2019-03-27 14:08:05 -0500

    extended functionality

--- src/spatial_hashing.py
+++ src/spatial_hashing.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 26 11:33:36 2019
+Created on Wed Mar 27 12:38:11 2019
 
 @author: ThomasTheisen
 """
@@ -10,109 +10,127 @@ import math
 
 class Grid():
     
-    def __init__(self, l, w):
-        self.grid = np.zeros((l,w))
+    def __init__(self, row_cells, column_cells):
+        self.rows = row_cells
+        self.columns = column_cells
+        self.grid = np.zeros((self.rows, self.columns))
+        self.paths = []
+        self.planes = []
+        self.hashes = {}
+        
+    def __repr__(self):
+        return repr(self.grid)
+        
+    def add_path(self, path):
+        self.paths.append(path)
+        
+    def add_plane(self, plane):
+        self.planes.append(plane)
+        
+    def gen_hash(self):
+        for index_path, path in enumerate(self.paths):
+            key = 'Path' + str(index_path)
+            self.hashes[key] = []
+            for index_plane, plane in enumerate(self.planes):
+                hash_value = hashfunc(path[index_plane][0], path[index_plane][1], plane)
+                self.hashes[key].append(hash_value)
                 
 class Path():
-    
-    def __init__(self, moves):
-        self.pos_start = np.array((np.random.randint(11), np.random.randint(11)))
-        self.chain = np.array(self.pos_start)
-        self.length = 1
-        
-    def move(self):
-        
-        if self.length == 1:
-            current_position = self.chain
-        else:
-            current_position = self.chain[-1]
-            
-        valid_position = True
-        while valid_position:
-            h_, v_ = np.random.randint(3), np.random.randint(3)
-            
-            if h_ == 0:
-                h_vec = -1
-            elif h_ == 1:
-                h_vec = 0
-            elif h_ == 2:
-                h_vec = 1
-                
-            if v_ == 0:
-                v_vec = -1
-            elif v_ == 1:
-                v_vec = 0
-            elif v_ == 2:
-                v_vec = 1
-                       
-            new_position = np.array((current_position[0] + h_vec, current_position[1] + v_vec))
-            
-            valid_position = invalid_position(new_position, 0, 10, 0, 10)
-        
-        self.chain = np.vstack((self.chain, new_position))
-        self.length += 1
-        
-        
-def invalid_position(position, x_lower, x_upper, y_lower, y_upper):
-    h, v = position[0], position[1]
-    
-    if h < x_lower or h > x_upper:
-        return True
-    elif v < y_lower or v > y_upper:
-        return True
-    else:
-        return False
-    
-def generate_plane(x_lower, x_upper, y_lower, y_upper):
-    p1 = np.array((0, np.random.randint(y_lower, y_upper+1)))
-    p2 = np.array((x_upper, np.random.randint(y_lower, y_upper+1)))
-    return p1, p2
 
+    def __init__(self):
+        self.path = list()
+        
+    def __repr__(self):
+        return repr(self.path)
+    
+    def __getitem__(self, index):
+        return self.path[index]
+          
+    def add_point(self, point):
+        self.path.append(point)
+        
+class Point():
+    
+    def __init__(self, latitude, longitude, time):
+        self.point = [latitude, longitude, time]
+        
+    def __repr__(self):
+        return repr(self.point)
+    
+    def __getitem__(self, index):
+        return self.point[index]
+    
+class Plane():
+    
+    def __init__(self, row_cells, column_cells, time):
+        self.plane = [[0, np.random.randint(row_cells)], [column_cells, np.random.randint(row_cells)], time]
 
-def distance(plane, x, y):
-    pp1, pp2 = plane
-    n = abs(pp2[0] - pp1[0]) * (pp1[1] - y) - (pp1[0] - x) * (pp2[1] - pp1[1])
-    d = math.sqrt(math.pow((pp2[0] - pp1[0]), 2) + math.pow((pp2[1] - pp1[1]),2))
-    return n/d
-
-def hashfunc(plane1, plane2, x, y):
-    p1_sign = distance(plane1, x, y)
-    p2_sign = distance(plane2, x, y)
-    if p1_sign > 0 and p2_sign > 0:
-        return 3
-    elif p1_sign > 0 and p2_sign < 0:
-        return 2
-    elif p1_sign < 0 and p2_sign > 0:
+    def __repr__(self):
+        return repr(self.plane)
+    
+    def __getitem__(self, index):
+        return self.plane[index]
+ 
+def hashfunc(x, y, plane):
+   
+    sign = distance(x, y, plane)
+    if sign > 0:
         return 1
     else: 
         return 0
     
-
-    
+def distance(x, y, plane):
+    pp1, pp2 = plane[0], plane[1]
+    n = abs(pp2[0] - pp1[0]) * (pp1[1] - y) - (pp1[0] - x) * (pp2[1] - pp1[1])
+    d = math.sqrt(math.pow((pp2[0] - pp1[0]), 2) + math.pow((pp2[1] - pp1[1]),2))
+    return n/d
+   
 
 if __name__ == "__main__":
-    g = Grid(10,10)
-    p1 = Path(5)
-    p2 = Path(5)
-    for _ in range(5):
-        p1.move()
-        p2.move()
+    dim_rows = 100
+    dim_columns = 100
     
-    plane1 = generate_plane(0, 10, 0, 10)
-    plane2 = generate_plane(0, 10, 0, 10)
-    plane3 = generate_plane(0, 10, 0, 10)
-    plane4 = generate_plane(0, 10, 0, 10)
-    plane5 = generate_plane(0, 10, 0, 10)
-    plane6 = generate_plane(0, 10, 0, 10)
-    plane7 = generate_plane(0, 10, 0, 10)
-    plane8 = generate_plane(0, 10, 0, 10)
-    plane9 = generate_plane(0, 10, 0, 10)
-    plane10 = generate_plane(0, 10, 0, 10)
-    plane11 = generate_plane(0, 10, 0, 10)
-    plane12 = generate_plane(0, 10, 0, 10)
-    plane_group1 = [plane1, plane2, plane3, plane4, plane5, plane6]
-    plane_group2 = [plane7, plane8, plane9, plane10, plane11, plane12]
+    grid = Grid(dim_rows, dim_columns)
+    path1 = Path()
+    path2 = Path()
     
-    #Both planes and points change throughout t time steps
-    for i in range(len(p1.chain)):
-        print(hashfunc(plane_group1[i], plane_group2[i], p1.chain[i][0], p1.chain[i][1]), end='')
+    p1 = Point(1,1,0)
+    p2 = Point(1,2,1)
+    p3 = Point(1,3,2)
+    p4 = Point(2,3,3)
+    
+    p5 = Point(4,1,0)
+    p6 = Point(4,2,1)
+    p7 = Point(5,2,2)
+    p8 = Point(5,3,3)
+    
+    plane0 = Plane(dim_rows, dim_columns, 0)
+    plane1 = Plane(dim_rows, dim_columns, 1)
+    plane2 = Plane(dim_rows, dim_columns, 2)
+    plane3 = Plane(dim_rows, dim_columns, 3)
+    
+    path1.add_point(p1)
+    path1.add_point(p2)
+    path1.add_point(p3)
+    path1.add_point(p4)
+    
+    path2.add_point(p5)
+    path2.add_point(p6)
+    path2.add_point(p7)
+    path2.add_point(p8)
+    
+    grid.add_path(path1)
+    grid.add_path(path2)
+    
+    grid.add_plane(plane0)
+    grid.add_plane(plane1)
+    grid.add_plane(plane2)
+    grid.add_plane(plane3)
+    
+    grid.gen_hash()
+    
+    
+    print(path1)
+    print(path2)
+    print(grid.hashes)
+        
