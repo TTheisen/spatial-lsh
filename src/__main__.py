ccad9c47636d1bb610fb92dc5bf75dd9a62e550d
Thomas Theisen <32493141+TTheisen@users.noreply.github.com>
2019-04-20 19:48:08 -0500

    Add files via upload

--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,8 @@ import utils
 if __name__ == "__main__":
 
     paths = []
-    for i in range(100):
+    for i in range(10):
         paths.append(utils.random_path())
     
-    utils.hash_all(paths)
-    
+    hashes = utils.hash_all(paths)
+    convolutions = utils.convolution_all(hashes, 5)
