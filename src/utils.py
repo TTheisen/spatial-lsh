d5276d093c23d1d2849de55356f034f22c1bd703
Thomas Theisen <32493141+TTheisen@users.noreply.github.com>
2019-04-20 19:45:59 -0500

    Add files via upload

--- src/utils.py
+++ src/utils.py
@@ -5,21 +5,31 @@ from partition import Partition
 import hashing
 import random
 
+variability = 10000
+
 def rn(upper):
     return random.randint(0, upper)
 
 def random_point():
-    return Point(rn(100), rn(100), rn(100))
+    return Point(rn(variability), rn(variability), rn(variability))
 
 def random_path():
-    p = Path(rn(100), [rn(100), rn(100)], [rn(100), rn(100)])
+    p = Path(rn(variability), [rn(variability), rn(variability)], [rn(variability), rn(variability)])
     return p.generate()
 
 def hash_all(paths):
+    coll = []
     for path in paths:
         h = []
         for p in path:
             partition = Partition(1, 10, 1, 0, 1000, 0, 1000)
             _, s = hashing.hash(p, partition.get_planes())
             h.append(s)
-        print(h)
+        coll.append(h)
+    return coll
+
+def convolution_all(hashes, padding):
+    c = []
+    for h in hashes:
+        c.append(hashing.convolution(h, padding))
+    return c  
